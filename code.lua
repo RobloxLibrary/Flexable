273092e1b6c0c44135081eaf1b28d387e67f01c9
CleverUser123 <94817071+CleverUser123@users.noreply.github.com>
2021-12-05 06:30:17 -0800

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,6 +1,6 @@
 -- Below script used for current page
--- game.HttpService:GetAsync("https://raw.githubusercontent.com/RobloxLibrary/Flexable/main/code.lua")
+-- loadstring(game.HttpService:GetAsync("https://raw.githubusercontent.com/RobloxLibrary/Flexable/main/code.lua"))()
 
 function noice(test)
-  print("Hello")
+  print(test)
 end
