1c5a4263db224f3639ef7e97c8df45cc02b60b77
CleverUser123 <94817071+CleverUser123@users.noreply.github.com>
2021-12-05 06:17:58 -0800

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,1 +1,6 @@
-loadstring(game:GetService("HttpService"):GetAsync("github.com"))
+-- Below script used for current page
+-- game.HttpService:GetAsync("https://raw.githubusercontent.com/RobloxLibrary/Flexable/main/code.lua")
+
+function noice(test)
+  print("Hello")
+end
